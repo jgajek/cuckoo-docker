10d17c806011bcaf29ab58d476ebb51ed40dc85d
Jacob Gajek <jacob.gajek@esentire.com>
2016-07-18 12:09:24 -0400

    Add 32-bit analysis VM to vSphere configuration. Run nginx as sandbox user.

--- 1.3-Optiv/startup.sh
+++ 1.3-Optiv/startup.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-# Adjust ownership of mounted volumes
-chown -R sandbox:www-data /opt/sandbox
-
-# Execute the supervisor daemon
 exec /usr/bin/supervisord -c /opt/sandbox/supervisor/supervisord.conf
